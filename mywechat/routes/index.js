31dc2bfaa07f48640d7f40422f13acca8e6dec9e
Duncan-tree-zhou <duncan.tree.zhou@gmail.com>
2018-08-08 23:28:14 +0800

    add wechat

--- mywechat/routes/index.js
+++ mywechat/routes/index.js
@@ -6,10 +6,10 @@ var weconfig = {
   appid: 'wxdef846e86bc42f84',
   appsecret: '63d772db5c2e69ad3298b481c408a1b2',
   encodingAESKey: 'iX2dUWhWvaDPGX6hiLGBs3CwqzUiifr5e5EdAwKnO7K'
-}
+};
 
 /* GET home page. */
-router.get('/', wechat(config, function(req, res, next) {
+router.get('/', wechat(weconfig, function(req, res, next) {
 	console.log(req.weixin);
 	var message = req.weixin;
 	//文本
