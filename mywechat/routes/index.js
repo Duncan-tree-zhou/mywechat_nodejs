76a9ff9b49126eed062d8ef06c5c3406c6755075
Duncan-tree-zhou <duncan.tree.zhou@gmail.com>
2018-08-08 22:53:54 +0800

    add wechat validation

--- mywechat/routes/index.js
+++ mywechat/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var weconfig = {
+  token: 'TREEZH0',
+  appid: 'wxdef846e86bc42f84',
+  appsecret: '63d772db5c2e69ad3298b481c408a1b2',
+  encodingAESKey: 'iX2dUWhWvaDPGX6hiLGBs3CwqzUiifr5e5EdAwKnO7K'
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', wechat(config, function(req, res, next) {
+	console.log(req.weixin);
+	var message = req.weixin;
+	//文本
+	if (message.Content === '1') {
+ 
+		res.reply('hehe');
+  }
+  
+  })
+);
 
 module.exports = router;
